8e67e73f3bd37de43165086d8cbb625d1daa5051
velina <velina@snowcat.hitronhub.home>
2017-05-07 00:26:06 -0700

    Switch from colormath to skimage for color computations to improve performance

--- target_selector.py
+++ target_selector.py
@@ -8,9 +8,7 @@ import pickle
 import sys
 import os.path
 
-from colormath.color_objects import sRGBColor, LabColor
-from colormath.color_conversions import convert_color
-from colormath.color_diff import delta_e_cie2000
+from skimage import color
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -23,21 +21,21 @@ class AreaFiller(object):
     def __init__(self, axes, img_array):
         nrows, ncols, _ = img_array.shape
 
+        # convert rgb values to lab
+        img_downscale = np.copy(img_array).astype(np.float32)/255
+        self.img_values = color.rgb2lab(img_downscale)
+        
         self.axes = axes
         self.target_col = None
         self.target_row = None
-        self.target_lab = None
-        self.img_array = img_array
+        self.target_color = None
         self.area_mask = np.zeros((nrows, ncols), dtype=np.bool)
         self.cid = axes.figure.canvas.mpl_connect('button_press_event', self)
 
-    def __meets_criteria(self, row, col):
+    def __is_similar_color(self, row, col):
         """ Determine if the color at the given point is within the desired range. """
-        r, g, b = self.img_array[row][col]
-        match_rgb = sRGBColor(r, g, b, True)
-        match_lab = convert_color(match_rgb, LabColor)
-
-        delta_e = delta_e_cie2000(match_lab, self.target_lab)
+        candidate_color = self.img_values[row][col]
+        delta_e = color.deltaE_ciede2000(candidate_color, self.target_color)
         return delta_e <= COLOR_THRESHOLD
 
     def __flood_fill(self):
@@ -45,7 +43,7 @@ class AreaFiller(object):
         Starting from a single point, move outwards to add all points that match
         the desired criteria.
         """
-        nrows, ncols, _ = self.img_array.shape
+        nrows, ncols, _ = self.img_values.shape
 
         to_check = set()
         to_check.add((self.target_row, self.target_col))
@@ -60,7 +58,7 @@ class AreaFiller(object):
             checked[row, col] = True
 
             # if it doesn't meet the criteria move on
-            if not self.__meets_criteria(row, col):
+            if not self.__is_similar_color(row, col):
                 continue
 
             self.area_mask[row][col] = True
@@ -81,11 +79,7 @@ class AreaFiller(object):
         """ Record the selected point as the location of target color and invoke a flood fill. """
         self.target_col = int(round(event.xdata))
         self.target_row = int(round(event.ydata))
-
-        # compute target color to compare to
-        r, g, b = self.img_array[self.target_row][self.target_col]
-        target_rgb = sRGBColor(r, g, b, True)
-        self.target_lab = convert_color(target_rgb, LabColor)
+        self.target_color = self.img_values[self.target_row][self.target_col]
 
         self.__flood_fill()
 
@@ -93,13 +87,6 @@ class AreaFiller(object):
 def handle_close(event):
     """ Handle close event for plot. """
     mask = event.canvas.figure.areafiller.area_mask
-
-    # hole out target region in image
-    target_indices = np.where(mask)
-    hole_img = event.canvas.figure.areafiller.img_array.copy()
-    hole_img[target_indices] = 0
-
-    # Save the pickle
     file_p = open('target_region.pkl', 'wb')
     pickle.dump(mask, file_p, -1)
     file_p.close()
